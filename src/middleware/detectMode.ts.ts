3873cfe6535bffe44731aca039f3896889d20cfd
Özgür Adem IŞIKLI <i.ozguradem@gmail.com>
2026-02-14 19:21:43 +0100

    fix: fixed mode selection

--- src/middleware/detectMode.ts.ts
+++ src/middleware/detectMode.ts.ts
@@ -4,15 +4,16 @@ import userService from "../services/user.service";
 const ROOT_DOMAINS = ["localhost", "rawfeed.social"];
 
 export async function detectMode(request: FastifyRequest, reply: FastifyReply) {
-  const hostname = request.hostname;
+  // This is the default mode all the time
+  request.mode = "root";
 
+  const hostname = request.hostname;
   if (ROOT_DOMAINS.includes(hostname) || hostname.endsWith(`.rawfeed.social`)) {
-    request.mode = "root";
     return;
   }
 
+  // Checking custom domain
   const user = await userService.getByCustomDomain(hostname);
-
   if (user && user.domain_verification_status === "verified") {
     request.mode = "custom";
     request.domainUser = user;
